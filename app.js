3711f3f4e70e2a94493b05694cbab4bd97ac6e6e
unknown <kc1.408596@gmail.com>
2020-09-17 16:54:12 +0530

    remove seeding

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ mongoose
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine","ejs");
 app.use(express.static(__dirname + "/public"));
-seedDB();
+// seedDB();
 
 // PASSPORT
 
