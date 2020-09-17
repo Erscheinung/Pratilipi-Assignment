9ad1acd7ab21776f3a9cd23e5d2ee25013698db7
unknown <kc1.408596@gmail.com>
2020-09-17 16:21:17 +0530

    add default view counts

--- seeds.js
+++ seeds.js
@@ -5,10 +5,14 @@ var data = [
     {
         title: "A Little Fable", 
         content: "'Alas,' said the mouse, 'the world gets smaller every day. At first it was so wide that I ran along and was happy to see walls appearing to my right and left, but these high walls converged so quickly that I’m already in the last room, and there in the corner is the trap into which I must run.' 'But you’ve only got to run the other way,' said the cat, and ate it.",
+        currentViewers: 1,
+        totalViews: 1
     },
     {
         title: "The Metamorphosis", 
         content: "As Gregor Samsa awoke one morning from uneasy dreams he found himself transformed in his bed into a gigantic insect.",
+        currentViewers: 1,
+        totalViews: 1
     },
 ]
  
@@ -30,5 +34,5 @@ function seedDB(){
         });
     }); 
 }
- 
+
 module.exports = seedDB;
